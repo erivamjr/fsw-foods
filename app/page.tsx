fd4a3ec275117ff3de621b1007dbc1d15e37661e
José Erivam <erivamdev@gmail.com>
2024-05-01 10:04:13 -0300

    :sparkles: feat: add product list and product item components

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,23 @@ import Image from "next/image";
 import { CategoryList } from "./_components/category-list";
 import { Header } from "./_components/header";
 import { Search } from "./_components/search";
+import { ProductList } from "./_components/product-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <Header />
+
       <div className="px-5 pt-6">
         <Search />
       </div>
+
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
+
       <div className="px-5 pt-6">
         <Image
           src={"/promo-banner-01.png"}
@@ -23,6 +29,20 @@ export default function Home() {
           sizes="100vm"
           quality={100}
         />
+      </div>
+
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant={"ghost"}
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
       </div>
     </>
   );
